1b1bd6c456fbf3126a25f50bb1dcff78137984bb
B10956063 <B10956063@mail.npust.edu.tw>
2022-03-07 10:59:25 +0800

    1059

---
+++ 220307/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _220307
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
