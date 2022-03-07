215755761773b07016611ec70003e1902059bb41
B10956063 <B10956063@mail.npust.edu.tw>
2022-03-07 11:58:34 +0800

    1158

--- 220307/Program.cs
+++ 220307/Program.cs
@@ -6,7 +6,57 @@ namespace _220307
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double meal_cost = 100;
+            int tip_percent = 15;
+            int tax_percent = 8;
+
+            Console.WriteLine("用餐總費用:" + meal_cost + tip_percent + tax_percent)
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Console.WriteLine("Hello World!");
+
+            // string test = "test";
+
+            //var aaa = "test";
+
+            //const string testConst = "aaa";
+
+            //testConst = "bb";
+
+            //int price = "100";
+
+            //bool isHappy = true;
+
+            //Console.WriteLine(!isHappy);
+
+            //Console.WriteLine("hi,網際網路程式設計");
+
+            //string test = $"你輸入的 是:{input}";
+
+            //Console.WriteLine("你輸入的是:" + input);
         }
     }
 }
